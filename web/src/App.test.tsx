4d5ab41d403b765f73bd488f271436eaa8b8b5f1
charlie-galb <galbraith.charlie@gmail.com>
2022-02-05 12:08:41 +0000

    Refactor to remove async warnings

--- web/src/App.test.tsx
+++ web/src/App.test.tsx
@@ -1,25 +1,45 @@
 import React from "react";
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import axios from 'axios';
 
+import BusDto from './types/BusDto';
 import App from './App';
+
+const testBus1: BusDto = {
+    id: 1,
+    busId: 111,
+    destination: 'Test Road',
+    minutesUntilArrival: 4,
+    nonOperationalDays: [1,2],
+}
+
+const testBus2: BusDto = {
+    id: 2,
+    busId: 222,
+    destination: 'Mock Avenue',
+    minutesUntilArrival: 2,
+    nonOperationalDays: [2,3],
+}
+
+const testBuses = [testBus1, testBus2]
 
 describe('BusCard', () => {
 
     beforeEach(() => {
-        axios.get = jest.fn();
+        axios.get = jest.fn().mockReturnValue(testBuses);
     });
 
-    it('Should retrieve bus times when the component mounts', () => {
+    it('Should retrieve bus times when the component mounts', async () => {
         render(<App />);
-        expect(axios.get).toBeCalledTimes(1);
+        await waitFor(() => expect(axios.get).toBeCalledTimes(1));
     });
+        
 
-    it('Should retrieve bus times every ten seconds', () => {
+    it('Should retrieve bus times every ten seconds', async () => {
         jest.useFakeTimers();
         render(<App />);
         jest.advanceTimersByTime(11000);
-        expect(axios.get).toBeCalledTimes(2);
+        await waitFor(() => expect(axios.get).toBeCalledTimes(2));
         jest.useRealTimers();
     });
 
